602e0df0f4ba9b03b16afbc8dd89c6e552ae9938
AaronOtu <aaronotu99@gmail.com>
2024-10-21 15:31:11 -0700

    adding new files

---
+++ lib/screens/mainn.dart
@@ -0,0 +1,54 @@
+//this main file is tutorial about the state management in flutter.
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My flutter App',
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Welcome Home"),
+            backgroundColor: Colors.blue,
+            elevation: 20.0,
+            centerTitle: true,
+          ),
+          drawer: Drawer(
+            backgroundColor: Colors.grey,
+          ),
+          body: MyStatefulWidget()),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatelessWidgetState();
+}
+
+class _MyStatelessWidgetState extends State<MyStatefulWidget> {
+  bool liked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: [
+      ListTile(
+        trailing: IconButton(
+          onPressed: () {
+          setState(() => liked = !liked);
+          }, 
+          icon: liked? (Icon(Icons.favorite)) : (Icon(Icons.favorite_outline))),
+        title: Text("Mouse Click"),
+        onTap: () {},
+      )
+    ]);
+  }
+}
